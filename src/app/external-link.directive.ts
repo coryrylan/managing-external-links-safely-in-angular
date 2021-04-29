b92fb156335c7eddd1b8a9cfe369cae7222d3ec5
coryrylan <splintercode.cb@gmail.com>
2021-04-28 22:06:03 -0500

    1 file modified

--- src/app/external-link.directive.ts
+++ src/app/external-link.directive.ts
@@ -1,28 +1,32 @@
-import { Directive, HostBinding, PLATFORM_ID, Inject, Input } from '@angular/core';
+import { Directive, HostBinding, PLATFORM_ID, Inject, Input, ElementRef } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: 'a[href]',
 })
 export class ExternalLinkDirective {
-  @HostBinding('attr.rel') relAttr = '';
-  @HostBinding('attr.target') targetAttr = '';
-  @HostBinding('attr.href') hrefAttr = '';
+  @HostBinding('attr.rel') relAttr = null;
+  @HostBinding('attr.target') targetAttr = null;
   @Input() href: string;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: string) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: string,
+    private elementRef: ElementRef) {}
 
   ngOnChanges() {
-    this.hrefAttr = this.href;
+    this.elementRef.nativeElement.href = this.href;
   
     if (this.isLinkExternal()) {
       // https://developers.google.com/web/tools/lighthouse/audits/noopener
       this.relAttr = 'noopener';
       this.targetAttr = '_blank';
+    } else {
+      this.relAttr = null;
+      this.targetAttr = null;
     }
   }
 
   private isLinkExternal() {
-    return isPlatformBrowser(this.platformId) && !this.href.includes(location.hostname);
+    return isPlatformBrowser(this.platformId) && !this.elementRef.nativeElement.hostname.includes(location.hostname);
   }
 }
