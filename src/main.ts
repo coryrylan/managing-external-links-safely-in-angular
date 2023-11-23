647601ad89a997032771930c192287f02d3a5300
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-22 21:02:12 -0600

    5 files modified, 4 added, 7 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ExternalLinkDirective } from './external-link.directive';
+import 'zone.js';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ExternalLinkDirective],
+  template: `
+    <h1>
+      <a href="https://coryrylan.com/blog/managing-external-links-safely-in-angular">
+        Managing External Links Safely in Angular
+      </a>
+    </h1>
 
-import { AppModule } from './app/app.module';
+    <a href="https://angular-sp7z3d.stackblitz.io#1">Link Internal</a>
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+    <br /><br />
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    <a href="https://{{dynamic}}#1">Link Internal Dynamic</a>
+
+    <br /><br />
+
+    <a href="https://coryrylan.com/blog/managing-external-links-safely-in-angular">Link External</a>
+  `,
+})
+export class App {
+  dynamic = 'angular-sp7z3d.stackblitz.io';
+}
+
+bootstrapApplication(App);
